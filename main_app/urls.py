90c9d3ffbbd8368e9d734777f05bf47c7d126cf0
asma2003 <asma200m2200@gmail.com>
2025-10-28 19:36:14 +0300

    connect all views to the URL

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import PlantsIndex,PlantDetail, SoilIndex, SoilDetail
+from .views import PlantsIndex,PlantDetail, SoilIndex, SoilDetail, ScheduleIndex, ScheduleDetail, FavoriteIndex, FavoriteDetail
 
 urlpatterns = [
     path('plants/', PlantsIndex.as_view(), name='plants_index'),
     path('plants/<int:plant_id>/', PlantDetail.as_view(), name='plant_detail'),
     path('soils/', SoilIndex.as_view(), name='soil_index'), 
-    path('soils/<int:soil_id>/', SoilDetail.as_view(), name='soil_detail')
+    path('soils/<int:soil_id>/', SoilDetail.as_view(), name='soil_detail'),
+    path('schedules/', ScheduleIndex.as_view(), name='schedules_index'),
+    path('schedules/<int:schedule_id>/', ScheduleDetail.as_view(), name='schedules_detail'),
+    path('favorites/', FavoriteIndex.as_view(), name='favorites_index'),
+    path('favorites/<int:favorite_id>/', FavoriteDetail.as_view(), name='favorites_detail'),
 ]
