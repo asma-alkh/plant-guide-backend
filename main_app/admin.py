c63af122e9b0ca0789f92070dc7d10d4263db51f
asma2003 <asma200m2200@gmail.com>
2025-10-28 22:15:14 +0300

    Add Category to tha admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Plant, Soil, Schedule, Favorite
+from .models import Plant, Soil, Schedule, Favorite, Category 
 # Register your models here.
 admin.site.register(Plant)
 admin.site.register(Soil)
 admin.site.register(Schedule)
 admin.site.register(Favorite)
+admin.site.register(Category)
